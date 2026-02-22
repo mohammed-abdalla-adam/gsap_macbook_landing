ec36c44963b441dc69ffac62a7461b065c29f266
mohammed-abdalla-adam <mazdasoft11@gmail.com>
2026-02-22 04:10:40 +0300

    Implement footer section & minor UI refinements

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,16 @@
-import {create} from 'zustand'
-
+import { create } from 'zustand';
 
 const useMacbookStore = create((set) => ({
     color: '#2e2c2e',
-    setColor: (color) => set({color}),
+    setColor: (color) => set({ color }),
 
     scale: 0.08,
-    setScale: (scale) => set({scale}),
+    setScale: (scale) => set({ scale }),
 
     texture: '/videos/feature-1.mp4',
-    setTexture: (texture) => set({texture}),
+    setTexture: (texture) => set({ texture }),
 
-    reset: () => set({color: '#2e2c2e', scale: 0.08, texture: '/videos/feature-1.mp4'}),
+    reset: () => set({ color: '#2e2c2e', scale: 0.08, texture: '/videos/feature-1.mp4' }),
 }))
 
 export default useMacbookStore;
